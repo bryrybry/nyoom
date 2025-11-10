893e5dcc86368e532ba5ab5552f50938c2ed13e6
bmyj0176 <bryong0176@gmail.com>
2025-11-11 01:52:14 +0800

    test gitlogs

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'nyooooom'),
+      home: const MyHomePage(title: 'nyoooooom'),
     );
   }
 }
