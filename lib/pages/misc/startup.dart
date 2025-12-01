87cc3d48539dfa240b0bca39ceb90751a65d1c7d
bry <bryanong.23@ichat.sp.edu.sg>
2025-12-01 15:52:21 +0800

    pbca fluttering: startup screen

--- lib/pages/misc/startup.dart
+++ lib/pages/misc/startup.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:nyoom/classes/colors.dart';
 import 'package:nyoom/main.dart';
 import 'package:nyoom/pages/bookmarks/bookmarks.dart';
-import 'package:nyoom/pages/misc/auth.dart';
 import 'package:nyoom/providers/navigation_provider.dart';
 
 class Startup extends ConsumerStatefulWidget implements PageSettings {
@@ -21,12 +21,13 @@ class _StartupState extends ConsumerState<Startup> {
   @override
   void initState() {
     super.initState();
-    print("test1");
-    init();
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await init();
+    });
   }
 
-  void init() {
-    print("test2");
+  Future<void> init() async {
+    await Future.delayed(const Duration(milliseconds: 500));
     ref.read(navigationProvider)?.call(Bookmarks());
   }
 
@@ -36,11 +37,12 @@ class _StartupState extends ConsumerState<Startup> {
       body: Stack(
         fit: StackFit.expand,
         children: [
+          Container(color: AppColors.black),
           Image.asset(
             'assets/images/nyoom_effect.gif',
             fit: BoxFit.cover, // fills the screen
           ),
-          Column(children: [const Text("hi")]),
+          Center(child: Image.asset('assets/images/nyoom.png')),
         ],
       ),
     );
